0cacc381d82dacde4493d7a272cc321e38db9d4c
MauritsDuel <mauritsduel06@outlook.com>
2021-05-12 17:06:14 +0200

    Klasse LevelTransmitter toegevoegd

---
+++ EduLab-Process-Simulator/LevelTransmitter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduLab_Process_Simulator
+{
+    public class Leveltransmitter
+    {
+        public string levelTransmitterName { get; set; }
+        public int levelTransmitterValue { get; set; }
+
+        public Leveltransmitter()
+        {
+
+        }
+
+        public int getStatus()
+        {
+            Console.WriteLine("{0}: is {1}", levelTransmitterName, levelTransmitterValue);
+            return levelTransmitterValue;
+        }
+
+    }
+}
