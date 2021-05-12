8941a6a594e3ec4b75ec6d80e0645f6fa8c368b3
MauritsDuel <mauritsduel06@outlook.com>
2021-05-12 12:24:42 +0200

    Klasse pump aangemaakt

---
+++ EduLab-Process-Simulator/Pump.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduLab_Process_Simulator
+{
+    public class Pump
+    {
+        public string pumpName { get; set; }
+        public bool pumpStatus { get; set; }
+
+        public Pump()
+        {            
+            
+        }
+
+        public bool setStatus(bool pumpOnOff)
+        {
+            if pumpOnOff == 1
+            {
+                pumpStatus = 1;
+            }
+            else
+            {
+                pumpStatus = 0;
+            }
+        }
+
+        public bool getStatus()
+        {
+            Console.WriteLine("{0}: is {1}", pumpName, pumpStatus);
+            return pumpStatus;
+        }
+
+
+    }
+}
