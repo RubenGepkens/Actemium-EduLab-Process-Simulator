6aa2e7c5c9742ea81a27409e70fe306431ddb3b0
Ruben Gepkens <gepkens23@gmail.com>
2021-05-28 12:17:35 +0200

    Added basic pump logic

--- EduLab-Process-Simulator/Pump.cs
+++ EduLab-Process-Simulator/Pump.cs
@@ -8,33 +8,80 @@ namespace EduLab_Process_Simulator
 {
     public class Pump
     {
-        public string pumpName { get; set; }
-        public bool pumpStatus { get; set; }
+        public string strPumpName { get; set; }
+        public bool blnPumpRunning { get; set; }
 
+        /// <summary>
+        /// Simulated pump.
+        /// </summary>
         public Pump()
-        {            
-            
-        }
-
-        public bool setStatus(bool pumpOnOff)
         {
-            if (pumpOnOff == true)
-            {
-                pumpStatus = true;
-            }
-            else
-            {
-                pumpStatus = false;
-            }
-            return pumpOnOff;
+            blnPumpRunning = false;
         }
 
-        public bool getStatus()
+        /// <summary>
+        /// Simulated pump.
+        /// </summary>
+        /// <param name="strObjectname"></param>
+        public Pump(string strObjectname)
         {
-            Console.WriteLine("{0}: is {1}", pumpName, pumpStatus);
-            return pumpStatus;
+            strPumpName = strObjectname;
+            blnPumpRunning = false;
         }
 
+        /// <summary>
+        /// Starts the pump.
+        /// </summary>
+        public void Start()
+        {
+            blnPumpRunning = true;
+        }
 
+        /// <summary>
+        /// Stops the pump
+        /// </summary>
+        public void Stop()
+        {
+            blnPumpRunning = false;
+        }
+
+        /// <summary>
+        /// Retrieves the pump status.
+        /// </summary>
+        /// <returns>true if the pump is running and false if it is stopped</returns>
+        public bool GetStatus()
+        {
+            return blnPumpRunning;
+        }
+
+        /// <summary>
+        /// Retrevies the pump status
+        /// </summary>
+        /// <returns>true if the pump is running</returns>
+        public bool IsRunning()
+        {
+            if (blnPumpRunning)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the pump status
+        /// </summary>
+        /// <returns>true if the pump is stopped</returns>
+        public bool IsStopped()
+        {
+            if (blnPumpRunning)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
     }
 }
